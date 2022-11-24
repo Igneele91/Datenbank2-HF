a4e30f63b7265697a37763eedb74f5daa34943e2
Igneele <69846931+Igneele91@users.noreply.github.com>
2022-11-24 17:40:20 +0100

    eee

--- Abrufe1.ps1
+++
@@ -1,8 +0,0 @@
-﻿# Created by Fabian and Arber
-
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-$Script = Invoke-RestMethod https://api.github.com/repos/Igneele91/Hi-1test/contents/Download_Files2.ps1?access_token=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 -Headers @{”Accept”= “application/vnd.github.v3.raw”}
-
-Invoke-Expression $Script
-
